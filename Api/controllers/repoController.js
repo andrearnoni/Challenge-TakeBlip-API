a225b418d6bf9bd54751615f739f4666c8391680
André Arnoni <andre_arnoni@yahoo.com.br>
2021-10-27 00:03:05 -0300

    inserida rota que busca por posição no array

--- Api/controllers/repoController.js
+++ Api/controllers/repoController.js
@@ -3,6 +3,21 @@ const repoService = require('../services/repoService');
 
 const router = express.Router();
 
+
+router.get('/:position', async (_req, res) => {
+  try {
+    const { position } = _req.params;
+    const getRepos = await repoService.getParams();
+
+    const getPosition = getRepos[position];
+
+    res.status(200).json(getPosition);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 router.get('/', async (_req, res) => {
   try {
     const getRepos = await repoService.getParams();
